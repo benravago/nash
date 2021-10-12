6ebda6904b16fd400523816586db2d356416a1f4
Ben Ravago <ben.ravago@aol.com>
2021-10-12 15:59:14 -0400

    jdk17 es.runtime.events

--- src/njs/es/runtime/events/RecompilationEvent.java
+++ src/njs/es/runtime/events/RecompilationEvent.java
@@ -6,11 +6,10 @@ import es.runtime.RecompilableScriptFunctionData;
 import es.runtime.RewriteException;
 
 /**
- * Subclass of runtime event for {@link RewriteException}. In order not
- * to leak memory, RewriteExceptions get their return value destroyed
- * and nulled out during recompilation. If we are running with event
- * logging enabled, we need to retain the returnValue, hence the extra
- * field
+ * Subclass of runtime event for {@link RewriteException}.
+ *
+ * In order not to leak memory, RewriteExceptions get their return value destroyed and nulled out during recompilation.
+ * If we are running with event logging enabled, we need to retain the returnValue, hence the extra field
  */
 public final class RecompilationEvent extends RuntimeEvent<RewriteException> {
 
@@ -21,14 +20,14 @@ public final class RecompilationEvent extends RuntimeEvent<RewriteException> {
    *
    * @param level            logging level
    * @param rewriteException rewriteException wrapped by this RuntimEvent
-   * @param returnValue      rewriteException return value - as we don't want to make
-   *     {@code RewriteException.getReturnValueNonDestructive()} public, we pass it as
-   *     an extra parameter, rather than querying the getter from another package.
+   * @param returnValue      rewriteException return value
+   *   - as we don't want to make {@code RewriteException.getReturnValueNonDestructive()} public,
+   *     we pass it as an extra parameter, rather than querying the getter from another package.
    */
-  public RecompilationEvent(final Level level, final RewriteException rewriteException, final Object returnValue) {
+  public RecompilationEvent(Level level, RewriteException rewriteException, Object returnValue) {
     super(level, rewriteException);
     assert Context.getContext().getLogger(RecompilableScriptFunctionData.class).isEnabled() :
-            "Unit test/instrumentation purpose only: RecompilationEvent instances should not be created without '--log=recompile', or we will leak memory in the general case";
+      "Unit test/instrumentation purpose only: RecompilationEvent instances should not be created without '--log=recompile', or we will leak memory in the general case";
     this.returnValue = returnValue;
   }
 
@@ -39,4 +38,5 @@ public final class RecompilationEvent extends RuntimeEvent<RewriteException> {
   public Object getReturnValue() {
     return returnValue;
   }
+
 }
--- src/njs/es/runtime/events/RuntimeEvent.java
+++ src/njs/es/runtime/events/RuntimeEvent.java
@@ -5,14 +5,13 @@ import es.runtime.options.Options;
 
 /**
  * Class for representing a runtime event, giving less global dependencies than logger.
- * Every {@link NativeDebug} object keeps a queue of RuntimeEvents that can be explored
- * through the debug API.
+ * Every {@link NativeDebug} object keeps a queue of RuntimeEvents that can be explored through the debug API.
  *
  * @param <T> class of the value this event wraps
  */
 public class RuntimeEvent<T> {
 
-  /** Queue size for the runtime event buffer */
+  // Queue size for the runtime event buffer
   public static final int RUNTIME_EVENT_QUEUE_SIZE = Options.getIntProperty("nashorn.runtime.event.queue.size", 1024);
 
   private final Level level;
@@ -39,24 +38,17 @@ public class RuntimeEvent<T> {
 
   @Override
   public String toString() {
-    final StringBuilder sb = new StringBuilder();
-
-    sb.append('[').
-            append(level).
-            append("] ").
-            append(value == null ? "null" : getValueClass().getSimpleName()).
-            append(" value=").
-            append(value);
-
-    return sb.toString();
+    return "[" + level + "] " +
+      (value == null ? "null" : getValueClass().getSimpleName()) +
+      " value=" + value;
   }
 
   /**
-   * Descriptor for this runtime event, must be overridden and
-   * implemented, e.g. "RewriteException"
+   * Descriptor for this runtime event, must be overridden and implemented, e.g. "RewriteException"
    * @return event name
    */
   public final Class<?> getValueClass() {
     return value.getClass();
   }
+
 }
