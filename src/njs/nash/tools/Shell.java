4ccc44a3a2b7977334eab5b65f89af86c95426bb
BenRavago <BenRavago@aol.com>
2021-12-30 14:41:46 -0500

    simpler shell

--- src/njs/nash/tools/Shell.java
+++ src/njs/nash/tools/Shell.java
@@ -2,34 +2,20 @@ package nash.tools;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.io.StreamTokenizer;
-import java.io.StringReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 import es.codegen.Compiler;
 import es.codegen.Compiler.CompilationPhases;
-import es.ir.Expression;
 import es.objects.Global;
 import es.objects.NativeSymbol;
-import es.objects.annotations.Attribute;
 import es.parser.Parser;
 import es.runtime.Context;
 import es.runtime.ErrorManager;
 import es.runtime.JSType;
-import es.runtime.Property;
 import es.runtime.ScriptFunction;
 import es.runtime.ScriptObject;
 import es.runtime.ScriptRuntime;
@@ -46,12 +32,6 @@ import static es.runtime.Source.sourceFor;
  * Command line Shell for processing JavaScript files.
  */
 public class Shell {
-
-  // Resource name for properties file
-  private static final String MESSAGE_RESOURCE = "nash.tools.resources.Shell";
-
-  // Shell message bundle.
-  protected static final ResourceBundle bundle = ResourceBundle.getBundle(MESSAGE_RESOURCE, Locale.getDefault());
 
   // Exit code for command line tool - successful
   public static final int SUCCESS = 0;
@@ -125,20 +105,11 @@ public class Shell {
     if (context == null) {
       return COMMANDLINE_ERROR;
     }
-
     var global = context.createGlobal();
     var env = context.getEnv();
-
-    var files = args;
-    if (files.length == 0) {
-      return readEvalPrint(context, global);
-    }
-
-    if (env._compile_only) {
-      return compileScripts(context, global, files);
-    }
-
-    return runScripts(context, global, files);
+    return args.length == 0 ? readEvalPrint(context, global)
+         : env._compile_only ? compileScripts(context, global, args)
+                             : runScripts(context, global, args);
   }
 
   /**
@@ -152,152 +123,16 @@ public class Shell {
    * @return null if there are problems with option parsing.
    */
   static Context makeContext(InputStream in, OutputStream out, OutputStream err, String... args) {
-    var pout = out instanceof PrintStream ? (PrintStream) out : new PrintStream(out);
-    var perr = err instanceof PrintStream ? (PrintStream) err : new PrintStream(err);
-    var wout = new PrintWriter(pout, true);
-    var werr = new PrintWriter(perr, true);
 
+    // TODO: make PrintStream wrapper
+    var wout = new PrintWriter(out, true);
+    var werr = new PrintWriter(err, true);
     // Set up error handler.
     var errors = new ErrorManager(werr);
     // Set up options.
     var options = new Options(Option::getProperty);
 
-//  // parse options
-//  if (args != null) {
-//    try {
-//      var prepArgs = preprocessArgs(args);
-//      options.process(prepArgs);
-//    } catch (IllegalArgumentException e) {
-//      werr.println(bundle.getString("shell.usage"));
-//      // options.displayHelp(e);
-//      return null;
-//    }
-//  }
-    
-    var scripting = false;
-
-    // detect scripting mode by any source's first character being '#'
-    if (!scripting) {
-      for (var fileName : args) { // options.getFiles()
-        var firstFile = new File(fileName);
-        if (firstFile.isFile()) {
-          try (var fr = new FileReader(firstFile)) {
-            var firstChar = fr.read();
-            // starts with '#
-            if (firstChar == '#') {
-              scripting = true;
-              break;
-            }
-          } catch (IOException e) {
-            // ignore this. File IO errors will be reported later anyway
-          }
-        }
-      }
-    }
-
     return new Context(options, errors, wout, werr, Thread.currentThread().getContextClassLoader());
-  }
-
-  /**
-   * Preprocess the command line arguments passed in by the shell.
-   *
-   * This method checks, for the first non-option argument, whether the file denoted by it begins with a shebang line.
-   * If so, it is assumed that execution in shebang mode is intended.
-   * The consequence of this is that the identified script file will be treated as the <em>only</em> script file, and all subsequent arguments will be regarded as arguments to the script.
-   * <p>
-   * This method canonicalizes the command line arguments to the form {@code <options> <script> -- <arguments>} if a shebang script is identified.
-   * On platforms that pass shebang arguments as single strings, the shebang arguments will be broken down into single arguments; whitespace is used as separator.
-   * <p>
-   * Shebang mode is entered regardless of whether the script is actually run directly from the shell, or indirectly via the {@code njs} executable.
-   * It is the user's / script author's responsibility to ensure that the arguments given on the shebang line do not lead to a malformed argument sequence.
-   * In particular, the shebang arguments should not contain any whitespace for purposes other than separating arguments, as the different platforms deal with whitespace in different and incompatible ways.
-   * <p>
-   * @implNote Example:<ul>
-   * <li>Shebang line in {@code script.js}: {@code #!/path/to/njs --language=es6}</li>
-   * <li>Command line: {@code ./script.js arg2}</li>
-   * <li>{@code args} array passed to Nashorn: {@code --language=es6,./script.js,arg}</li>
-   * <li>Required canonicalized arguments array: {@code --language=es6,./script.js,--,arg2}</li>
-   * </ul>
-   *
-   * @param args the command line arguments as passed into Nashorn.
-   * @return the passed and possibly canonicalized argument list
-   */
-  static String[] preprocessArgs(String... args) {
-    if (args.length == 0) {
-      return args;
-    }
-
-    var processedArgs = new ArrayList<String>();
-    processedArgs.addAll(Arrays.asList(args));
-
-    // Nash supports passing multiple shebang arguments.
-    // On platforms that pass anything following the shebang interpreter notice as one argument,
-    // the first element of the argument array needs to be special-cased as it might actually contain several arguments.
-    // Mac OS X splits shebang arguments, other platforms don't.
-    // This special handling is also only necessary if the first argument actually starts with an option.
-    if (args[0].startsWith("-") && !System.getProperty("os.name", "generic").startsWith("Mac OS X")) {
-      processedArgs.addAll(0, tokenizeString(processedArgs.remove(0)));
-    }
-
-    var shebangFilePos = -1; // -1 signifies "none found"
-    // identify a shebang file and its position in the arguments array (if any)
-    for (var i = 0; i < processedArgs.size(); ++i) {
-      var a = processedArgs.get(i);
-      if (!a.startsWith("-")) {
-        var p = Paths.get(a);
-        var l = "";
-        try (var r = Files.newBufferedReader(p)) {
-          l = r.readLine();
-        } catch (IOException ioe) {
-          // ignore
-        }
-        if (l != null && l.startsWith("#!")) {
-          shebangFilePos = i;
-        }
-        // We're only checking the first non-option argument.
-        // If it's not a shebang file, we're in normal execution mode.
-        break;
-      }
-    }
-    if (shebangFilePos != -1) {
-      // Insert the argument separator after the shebang script file.
-      processedArgs.add(shebangFilePos + 1, "--");
-    }
-    return processedArgs.stream().toArray(String[]::new);
-  }
-
-  public static List<String> tokenizeString(String str) {
-    var tokenizer = new StreamTokenizer(new StringReader(str));
-    tokenizer.resetSyntax();
-    tokenizer.wordChars(0, 255);
-    tokenizer.whitespaceChars(0, ' ');
-    tokenizer.commentChar('#');
-    tokenizer.quoteChar('"');
-    tokenizer.quoteChar('\'');
-    var tokenList = new ArrayList<String>();
-    var toAppend = new StringBuilder();
-    while (nextToken(tokenizer) != StreamTokenizer.TT_EOF) {
-      var s = tokenizer.sval;
-      // The tokenizer understands about honoring quoted strings and recognizes them as one token that possibly contains multiple space-separated words.
-      // It does not recognize quoted spaces, though, and will split after the escaping \ character.
-      // This is handled here.
-      if (s.endsWith("\\")) {
-        // omit trailing \, append space instead
-        toAppend.append(s.substring(0, s.length() - 1)).append(' ');
-      } else {
-        tokenList.add(toAppend.append(s).toString());
-        toAppend.setLength(0);
-      }
-    }
-    if (toAppend.length() != 0) {
-      tokenList.add(toAppend.toString());
-    }
-    return tokenList;
-  }
-
-  static int nextToken(StreamTokenizer tokenizer) {
-    try { return tokenizer.nextToken(); }
-    catch (IOException ioe) { return StreamTokenizer.TT_EOF; }
   }
 
   /**
@@ -364,7 +199,7 @@ public class Shell {
       if (globalChanged) {
         Context.setGlobal(global);
       }
-      addScriptingBuiltins(global);
+      ShellFunctions.init(global);
       var errors = context.getErrorManager();
       // For each file on the command line.
       for (var fileName : files) {
@@ -418,39 +253,6 @@ public class Shell {
   }
 
   /**
-   * Parse potentially partial code and keep track of the start of last expression.
-   * This 'partial' parsing support is meant to be used for code-completion.
-   *
-   * @param context the nashorn context
-   * @param code code that is to be parsed
-   * @return the start index of the last expression parsed in the (incomplete) code.
-   */
-  public final int getLastExpressionStart(Context context, String code) {
-    var exprStart = new int[] {-1};
-
-    var p = new Parser(context.getEnv(), sourceFor("<partial_code>", code), new Context.ThrowErrorManager()) {
-      @Override
-      protected Expression expression() {
-        exprStart[0] = this.start;
-        return super.expression();
-      }
-      @Override
-      protected Expression assignmentExpression(boolean noIn) {
-        exprStart[0] = this.start;
-        return super.assignmentExpression(noIn);
-      }
-    };
-
-    try {
-      p.parse();
-    } catch (Exception ignored) {
-      // throw any parser exception, but we are partial parsing anyway
-    }
-
-    return exprStart[0];
-  }
-
-  /**
    * read-eval-print loop for Nash shell.
    *
    * @param context the nashorn context
@@ -458,19 +260,17 @@ public class Shell {
    * @return return code
    */
   protected int readEvalPrint(Context context, Global global) {
-    var prompt = bundle.getString("shell.prompt");
+    var prompt = "nash>";
     var in = new BufferedReader(new InputStreamReader(System.in));
     var err = context.getErr();
     var oldGlobal = Context.getGlobal();
     var globalChanged = (oldGlobal != global);
-    var env = context.getEnv();
 
     try {
       if (globalChanged) {
         Context.setGlobal(global);
       }
-      addScriptingBuiltins(global);
-      addShellBuiltins(global);
+      ShellFunctions.init(global);
       for (;;) {
         err.print(prompt);
         err.flush();
@@ -552,31 +352,6 @@ public class Shell {
   static boolean isArrayWithDefaultToString(Object result, Global global) {
     return result instanceof ScriptObject sobj && sobj.isArray() &&
            sobj.get("toString") == global.getArrayPrototype().get("toString");
-  }
-
-  /**
-   * from es.object.Global
-   * Adds njs shell interactive mode builtin functions to global scope.
-   */
-  void addShellBuiltins(Global global) {
-    ScriptFunction value;
-    System.out.println("addShellBuiltin "+global);
-    value = ScriptFunction.createBuiltin("input", ShellFunctions.INPUT);
-    global.addOwnProperty("input", Attribute.NOT_ENUMERABLE, value);
-    value = ScriptFunction.createBuiltin("evalinput", ShellFunctions.EVALINPUT);
-    global.addOwnProperty("evalinput", Attribute.NOT_ENUMERABLE, value);
-  }
-
-  void addScriptingBuiltins(Global global) {
-    ScriptFunction value;
-    value = ScriptFunction.createBuiltin("print", ShellFunctions.PRINTLN);
-    global.addOwnProperty("print", Attribute.NOT_ENUMERABLE, value);
-    value = ScriptFunction.createBuiltin("warn", ShellFunctions.ERRORLN);
-    global.addOwnProperty("warn", Attribute.NOT_ENUMERABLE, value);
-    value = ScriptFunction.createBuiltin("exit", ShellFunctions.EXIT);
-    global.addOwnProperty("exit", Attribute.NOT_ENUMERABLE, value);
-    value = ScriptFunction.createBuiltin("quit", ShellFunctions.EXIT);
-    global.addOwnProperty("quit", Attribute.NOT_ENUMERABLE, value);
   }
 
 }
--- src/njs/nash/tools/ShellFunctions.java
+++ src/njs/nash/tools/ShellFunctions.java
@@ -5,7 +5,10 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 
 import es.objects.Global;
+import es.objects.annotations.Attribute;
 import es.runtime.JSType;
+import es.runtime.ScriptFunction;
+
 import static es.lookup.Lookup.MH;
 import static es.runtime.ScriptRuntime.UNDEFINED;
 import java.io.UncheckedIOException;
@@ -16,62 +19,9 @@ import java.io.Writer;
  */
 final class ShellFunctions {
 
-  // Handle to implementation of {@link ShellFunctions#input} - Nashorn extension
-  public static final MethodHandle INPUT =
-    findOwnMH("input", Object.class, Object.class, Object.class, Object.class);
+  public static final MethodHandle ERRORLN = findOwnMH("errorln", Object.class, Object.class, Object[].class);
+  public static final MethodHandle PRINTLN = findOwnMH("println", Object.class, Object.class, Object[].class);
 
-  /** Handle to implementation of {@link ShellFunctions#evalinput} - Nashorn extension */
-  public static final MethodHandle EVALINPUT =
-    findOwnMH("evalinput", Object.class, Object.class, Object.class, Object.class);
-
-  /**
-   * Nashorn extension: global.input (shell-interactive-mode-only)
-   * Read one or more lines of input from the standard input till the given end marker is seen in standard input.
-   *
-   * @param self   self reference
-   * @param endMarker String used as end marker for input
-   * @param prompt String used as input prompt
-   *
-   * @return line that was read
-   *
-   * @throws IOException if an exception occurs
-   */
-  public static Object input(Object self, Object endMarker, Object prompt) throws IOException {
-    var endMarkerStr = (endMarker != UNDEFINED) ? JSType.toString(endMarker) : "";
-    var promptStr = (prompt != UNDEFINED) ? JSType.toString(prompt) : ">> ";
-    var buf = new StringBuilder();
-    for (;;) {
-      var line = ScriptingFunctions.readLine(promptStr);
-      if (line == null || line.equals(endMarkerStr)) {
-        break;
-      }
-      buf.append(line);
-      buf.append('\n');
-    }
-    return buf.toString();
-  }
-
-  /**
-   * Nashorn extension:
-   * Reads zero or more lines from standard input and evaluates the concatenated string as code
-   *
-   * @param self self reference
-   * @param endMarker String used as end marker for input
-   * @param prompt String used as input prompt
-   *
-   * @return output from evaluating the script
-   *
-   * @throws IOException if an exception occurs
-   */
-  public static Object evalinput(Object self, Object endMarker, Object prompt) throws IOException {
-    return Global.eval(self, input(self, endMarker, prompt));
-  }
-
-  public static final MethodHandle ERRORLN =
-    findOwnMH("errorln", Object.class, Object.class, Object[].class);
-  public static final MethodHandle PRINTLN =
-    findOwnMH("println", Object.class, Object.class, Object[].class);
-  
   /**
    * Global warn implementation - Nashorn extension
    * @param self    scope
@@ -111,8 +61,7 @@ final class ShellFunctions {
     return UNDEFINED;
   }
 
-  public static final MethodHandle EXIT =
-    findOwnMH("exit", Object.class, Object.class, Object.class);
+  public static final MethodHandle EXIT = findOwnMH("exit", Object.class, Object.class, Object.class);
 
   /**
    * Global exit and quit implementation - Nashorn extension: perform a {@code System.exit} call from the script
@@ -124,7 +73,19 @@ final class ShellFunctions {
     System.exit(JSType.toInt32(code));
     return UNDEFINED;
   }
-  
+
+  static void init(Global global) {
+    ScriptFunction value;
+    value = ScriptFunction.createBuiltin("print", ShellFunctions.PRINTLN);
+    global.addOwnProperty("print", Attribute.NOT_ENUMERABLE, value);
+    value = ScriptFunction.createBuiltin("warn", ShellFunctions.ERRORLN);
+    global.addOwnProperty("warn", Attribute.NOT_ENUMERABLE, value);
+    value = ScriptFunction.createBuiltin("exit", ShellFunctions.EXIT);
+    global.addOwnProperty("exit", Attribute.NOT_ENUMERABLE, value);
+    value = ScriptFunction.createBuiltin("quit", ShellFunctions.EXIT);
+    global.addOwnProperty("quit", Attribute.NOT_ENUMERABLE, value);
+  }
+
   private static MethodHandle findOwnMH(String name, Class<?> rtype, Class<?>... types) {
     return MH.findStatic(MethodHandles.lookup(), ShellFunctions.class, name, MH.type(rtype, types));
   }
